5f24f9f9c41ce6051a05f283ae84bb7f254b830e
growupboron <shankhoghosh123@gmail.com>
2019-04-14 05:14:02 +0530

    First commit

---
+++ test.py
@@ -0,0 +1,26 @@
+from luma.core.interface.serial import i2c,spi
+from luma.core.render import canvas
+from luma.oled.device import ssd1331
+import os
+from PIL import Image
+import time 
+serial=spi(device=0,port=0)
+device=ssd1331(serial)
+lis=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','T','U','V','W','X','Y','Z']
+#for i in lis:
+	#img=Image.open(os.getcwd()+'/'+i+'.png').convert("RGBA")
+	#fff=Image.new(img.mode,img.size,(255,)*4)
+	#background=Image.new("RGBA",device.size,"white")
+	#posn=((device.width-img.width)//2,0)
+	#background.paste(img,posn)
+	#device.display(background.convert(device.mode))
+	#time.sleep(5)
+	#device.clear()
+def text():
+	with canvas(device) as draw:
+		draw.rectangle(device.bounding_box,outline="white",fill="black")
+		draw.text((30,40),"A",fill="white")
+text()
+time.sleep(5)
+#print(dir(device))
+
